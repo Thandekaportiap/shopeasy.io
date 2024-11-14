44cc449c498b7bba5c5d2c697efd546c43ae8473
Thandekaportiap <thandekaportiap05@gmail.com>
2024-11-14 11:55:16 +0200

    chore: change the hero image

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import { TiShoppingCart } from 'react-icons/ti';
 
 const HomePage = () => {
   let image = require('../assets/images(1).png');
+  let image1 = require('../assets/laptop-removebg-preview.png');
+  let image2 = require('../assets/blackSM-removebg-preview.png');
   return (
     <>
       <section className='w-full h-screen flex flex-col lg:flex-row items-center justify-between px-4 lg:px-12 text-[#675953]'>
@@ -30,37 +32,13 @@ const HomePage = () => {
 
         <div className="lg:ml-auto lg:text-right mt-10 lg:mt-0">
   <div className="relative z-10 inline-block pt-11 lg:pt-0 w-full h-full max-w-lg"> {/* Set max width here */}
-    <img
-      src={image}
+  </div>
+  <img
+      src={image1}
       alt="hero"
       className="rounded-tl-3xl w-full h-auto" // Change this to w-full for full width
     />
-    <span className="absolute -bottom-8 -left-8 z-[-1]">
-      <svg
-        width="120" // Increase width for the SVG if needed
-        height="120" // Increase height for the SVG if needed
-        viewBox="0 0 120 120" // Adjust viewBox if you change width and height
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {Array.from({ length: 5 }, (_, i) => (
-          <circle key={i} cx="3.5" cy={(i + 1) * 22} r="3.5" fill="#675953" />
-        ))}
-        {Array.from({ length: 5 }, (_, i) => (
-          <circle key={i + 5} cx="25.5" cy={(i + 1) * 22} r="3.5" fill="#675953" />
-        ))}
-        {Array.from({ length: 5 }, (_, i) => (
-          <circle key={i + 10} cx="47.5" cy={(i + 1) * 22} r="3.5" fill="#675953" />
-        ))}
-        {Array.from({ length: 5 }, (_, i) => (
-          <circle key={i + 15} cx="69.5" cy={(i + 1) * 22} r="3.5" fill="#675953" />
-        ))}
-        {Array.from({ length: 5 }, (_, i) => (
-          <circle key={i + 20} cx="91.5" cy={(i + 1) * 22} r="3.5" fill="#675953" />
-        ))}
-      </svg>
-    </span>
-  </div>
+   
 </div>
 
       </section>
